691bd26f1ceabb15bf885e0f3583a6e9aac902d5
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2023-10-29 18:55:39 +0530

    Initial Build Ready

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,35 @@
+import Logo from "@/assets/temp.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} className="h-16 rounded-lg" />
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+            orci ut habitant laoreet. Iaculis tristique.
+          </p>
+          <p>
+            © 2023 All Right Reserved – Designed and Developed by Copzip
+            Technology Private Limited
+          </p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Trainers</p>
+          <p className="my-5">Facilities</p>
+          <p>Get Us Know</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">New Delhi</p>
+          <p>(+91)-1234567890</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
